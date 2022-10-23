e929be8f1d3c9adf5914def1889a3b2233d3bf86
techy <60556787+NerdyTechy@users.noreply.github.com>
2022-10-23 23:32:43 +0100

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.all('/', (req, res) => { res.status(400).json({ success: false, message: "No endpoint" }); });
+app.all('*', (req, res) => { res.status(404).json({ success: false, message: "Unknown endpoint" }); });
+
+app.listen(2028, () => console.log(`Running on port 2028.`));
