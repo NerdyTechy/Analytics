58d1827239ff27176dc49195eead2fd2c805bdf2
techy <60556787+NerdyTechy@users.noreply.github.com>
2022-10-23 23:36:07 +0100

    Require dotenv

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 app.use(express.json());
+
+app.post('/melody', (req, res) => {
+    // do database stuff
+    return res.status(200).json({ success: true, message: "Success" });
+});
 
 app.all('/', (req, res) => { res.status(400).json({ success: false, message: "No endpoint" }); });
 app.all('*', (req, res) => { res.status(404).json({ success: false, message: "Unknown endpoint" }); });
