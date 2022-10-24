bedf2eca9e1b543ad37e2cdeae4b2aa94daec0a8
techy <techy@nerdytechy.dev>
2022-10-24 13:09:59 +0100

    Add initialService column to DB, remove debug code

--- src/utils.js
+++ src/utils.js
@@ -7,8 +7,6 @@ const user = process.env.sql_user;
 const pass = process.env.sql_pass;
 const data = process.env.sql_data;
 
-console.log(host);
-
 const pool = mysql.createPool({
     connectionLimit: 100,
     host: host,
@@ -22,7 +20,7 @@ const pool = mysql.createPool({
  * Generate database table if it doesn't already exist.
  */
 function initDatabase() {
-    pool.query("CREATE TABLE IF NOT EXISTS `statistics` (`id` BIGINT NOT NULL AUTO_INCREMENT, `identifier` VARCHAR(64), `service` TINYTEXT, `launches` BIGINT, `lastLaunch` TIMESTAMP, PRIMARY KEY (`id`));");
+    pool.query("CREATE TABLE IF NOT EXISTS `statistics` (`id` BIGINT NOT NULL AUTO_INCREMENT, `identifier` VARCHAR(64), `service` TINYTEXT, `initialService` TINYTEXT, `launches` BIGINT, `lastLaunch` TIMESTAMP, PRIMARY KEY (`id`));");
 }
 
 /**
@@ -33,7 +31,7 @@ function updateMelodyClient(identifier){
     initDatabase();
     pool.query(`SELECT * FROM statistics WHERE \`identifier\`="${identifier}";`, function (err, res) {
         if (!res || res.length != 1){
-            pool.query(`INSERT INTO statistics (\`identifier\`, \`launches\`, \`service\`, \`lastLaunch\`) VALUES ("${identifier}", 1, "melody", "${moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')}")`);
+            pool.query(`INSERT INTO statistics (\`identifier\`, \`launches\`, \`service\`, \`initialService\`, \`lastLaunch\`) VALUES ("${identifier}", 1, "melody", "melody", "${moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')}")`);
         } else {
             pool.query(`UPDATE statistics SET \`launches\`=${res[0].launches + 1},\`service\`="melody",\`lastLaunch\`="${moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')}";`);
         }
